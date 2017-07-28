affcddc6603b3422a747f546586dc8232f5c7ae1
YKCzoli <yuriy.czoli@gmail.com>
2017-07-27 22:53:01 -0400

    Spin hack for wms rendering

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+var target = document.getElementById('map');
+var opts = {
+  lines: 9 // The number of lines to draw
+, length: 0 // The length of each line
+, width: 11 // The line thickness
+, radius: 18 // The radius of the inner circle
+, scale: 0.75 // Scales overall size of the spinner
+, corners: 1 // Corner roundness (0..1)
+, color: '#000' // #rgb or #rrggbb or array of colors
+, opacity: 0.15 // Opacity of the lines
+, rotate: 65 // The rotation offset
+, direction: 1 // 1: clockwise, -1: counterclockwise
+, speed: 1.2 // Rounds per second
+, trail: 44 // Afterglow percentage
+, fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+, zIndex: 2e9 // The z-index (defaults to 2000000000)
+, className: 'spinner' // The CSS class to assign to the spinner
+, top: '51%' // Top position relative to parent
+, left: '50%' // Left position relative to parent
+, shadow: false // Whether to render a shadow
+, hwaccel: false // Whether to use hardware acceleration
+, position: 'absolute' // Element positioning
+}
+var spinner = new Spinner(opts).spin(target);
+
 if (!('remove' in Element.prototype)) {
   Element.prototype.remove = function() {
     if (this.parentNode) {
@@ -33,6 +58,16 @@ map.on('load', function() {
         },
         'paint': {}
     }, 'water');
+});
+
+var counter = 0;
+map.on("render", function() {
+  if(map.loaded()) {
+    counter+=1;
+  }
+  if (counter == 20) {
+    spinner.stop();
+  }
 });
 
 var elevation_places = {
@@ -110,6 +145,7 @@ var elevation_places = {
         "name": "Barbeau Peak",
         "provterr": "Nunavut",
         "elevation": 2616,
+        "zoom": 7,
         "description": "Barbeau Peak is a mountain in Qikiqtaaluk, Nunavut, Canada. Located on Ellesmere Island within Quttinirpaaq National Park, it is the highest mountain in Nunavut and the Canadian Arctic. The mountain was named in 1969 for Dr. Marius Barbeau (1883-1969), a Canadian anthropologist whose research into First Nations and Inuit cultures gained him international acclaim."
       }
     },
